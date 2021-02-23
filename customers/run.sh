b91a07f0815670df11c96f502a6e434dd495792d
Josh Long <josh@joshlong.com>
2021-02-22 23:02:43 -0800

    up

--- customers/run.sh
+++ customers/run.sh
@@ -2,7 +2,7 @@
 docker run \
     -e SPRING_PROFILES_ACTIVE=cloud \
     -e SPRING_R2DBC_URL=r2dbc:postgres://localhost/orders  \
-    -e SPRING_R2DBC_USERNAME=jlong \
+    -e SPRING_R2DBC_USERNAME=user \
     -e SPRING_R2DBC_PASSWORD=password
     docker.io/library/customers:0.0.1-SNAPSHOT
 
