8add70ddb96543554aa6e3132890b96039414918
Josh Long <josh@joshlong.com>
2021-02-22 22:53:17 -0800

    up

--- customers/src/main/java/com/example/customers/CustomersApplication.java
+++ customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -1,7 +1,27 @@
 package com.example.customers;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.Collections.singletonMap;
 
 @SpringBootApplication
 public class CustomersApplication {
@@ -10,4 +30,52 @@ public class CustomersApplication {
 		SpringApplication.run(CustomersApplication.class, args);
 	}
 
+
+}
+
+@RestController
+@RequiredArgsConstructor
+class CustomerRestController {
+
+	private final CustomerRepository repository;
+
+	@GetMapping("/customers")
+	Flux<Customer> get() {
+		return this.repository.findAll();
+	}
+
+}
+
+@RestController
+@RequiredArgsConstructor
+class AvailabilityRestController {
+
+	private final ApplicationContext context;
+
+	@PostMapping("/down")
+	void down() {
+		AvailabilityChangeEvent.publish(this.context, LivenessState.BROKEN);
+	}
+
+	@GetMapping("/slow")
+	Flux<Map<String, String>> slow() {
+		return Flux
+			.fromStream(Stream.generate(() -> singletonMap("greetings", "Hello, world @ " + Instant.now() + "!")))
+			.take(100)
+			.delayElements(Duration.ofSeconds(1));
+	}
+
+}
+
+interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+
+	@Id
+	private Integer id;
+	private String name;
 }
