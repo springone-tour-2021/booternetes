eef48dcde7dcc03d471f30d47b7fd213c8285756
Cora Iberkleid <ciberkleid@gmail.com>
2021-03-05 08:52:57 -0500

    add sleuth, remove availabilityeventcontroller, removed unnecessary properties

--- customers/src/main/java/com/example/customers/CustomersApplication.java
+++ customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -6,27 +6,15 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.availability.AvailabilityChangeEvent;
-import org.springframework.boot.availability.LivenessState;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Map;
-import java.util.stream.Stream;
-
-import static java.util.Collections.singletonMap;
 
 @SpringBootApplication
 public class CustomersApplication {
@@ -57,27 +45,6 @@ class CustomerRestController {
 	@GetMapping("/customers")
 	Flux<Customer> get() {
 		return this.repository.findAll();
-	}
-
-}
-
-@RestController
-@RequiredArgsConstructor
-class AvailabilityRestController {
-
-	private final ApplicationContext context;
-
-	@PostMapping("/down")
-	void down() {
-		AvailabilityChangeEvent.publish(this.context, LivenessState.BROKEN);
-	}
-
-	@GetMapping("/slow")
-	Flux<Map<String, String>> slow() {
-		return Flux
-			.fromStream(Stream.generate(() -> singletonMap("greetings", "Hello, world @ " + Instant.now() + "!")))
-			.take(100)
-			.delayElements(Duration.ofSeconds(1));
 	}
 
 }
