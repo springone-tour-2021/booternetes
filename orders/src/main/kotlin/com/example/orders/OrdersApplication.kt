b284b4c6583b27ab4e0d1d634246e4f56639e4bb
Josh Long <josh@joshlong.com>
2021-03-08 12:59:52 -0800

    orders

---
+++ orders/src/main/kotlin/com/example/orders/OrdersApplication.kt
@@ -0,0 +1,37 @@
+package com.example.orders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+
+@SpringBootApplication
+class OrdersApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrdersApplication>(*args)
+}
+
+data class Order(var id: Int, var customerId: Int)
+
+@Controller
+class OrdersRSocketController {
+
+    private val db = mutableMapOf<Int, Collection<Order>>()
+
+    init {
+        for (customerId in 0..3) {
+            this.db[customerId] = randomOrdersFor(customerId)
+        }
+        this.db.forEach { (customerId, orders) -> println("the customerId is #${customerId} and the orders are ${orders}") }
+    }
+
+    private fun randomOrdersFor(id: Int): Collection<Order> {
+        val listOfOrders = mutableListOf<Order>()
+        val maxCount: Int = (Math.random() * 1000).toInt()
+        for (count in 1..maxCount) {
+            listOfOrders.add(Order(count, id))
+        }
+        return listOfOrders
+    }
+
+}
